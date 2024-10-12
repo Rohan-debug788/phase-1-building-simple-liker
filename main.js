f8055a199093163d7edcd214432f22ec73efed3b
Rohan Kibet <degaullerohan@gmail.com>
2024-10-12 16:47:17 +0300

    add complete lab

--- main.js
+++ main.js
@@ -5,7 +5,39 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+// Function to handle heart click
+function handleHeartClick(event) {
+  const heart = event.target;
 
+  mimicServerCall()
+    .then(() => {
+    
+      if (heart.innerText === '♡') {
+        heart.innerText = '♥'; 
+        heart.classList.add('activated-heart'); 
+      } else {
+        heart.innerText = '♡'; 
+        heart.classList.remove('activated-heart');
+      }
+    })
+    .catch(() => {
+      
+      const errorModal = document.getElementById('modal');
+      errorModal.classList.remove('hidden'); 
+      errorModal.querySelector('#modal-message').innerText = 'Server error!';
+
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+// Add event listener to heart icons
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('like-glyph')) {
+    handleHeartClick(event);
+  }
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
